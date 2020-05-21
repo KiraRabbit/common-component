f888fc1cacc46631810347c75b50b6ee5d677d2e
chenghao.zhou <chenghao.zhou@jkohjkng.top>
2020-05-21 21:36:43 +0800

    ** 公共组件 1.0.0 统一入口日志打印 **

---
+++ src/main/java/com/guuidea/common/component/utils/CollectionUtil.java
@@ -0,0 +1,24 @@
+package com.guuidea.common.component.utils;
+
+import java.util.Map;
+
+/**
+ * @author chenghao.zhou
+ * @date 2020/5/21 16:42
+ */
+public class CollectionUtil {
+
+    public static String changeMapToString(Map<String, String[]> map) {
+        StringBuffer params = new StringBuffer();
+
+        if (map != null && map.size() != 0) {
+            for (Map.Entry<String, String[]> entry : map.entrySet()) {
+                params.append(entry.getKey()).append(":").append(entry.getValue()[0]).append(",");
+            }
+            params.deleteCharAt(params.length()-1);
+        }
+
+        return params.toString();
+    }
+
+}
